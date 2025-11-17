6c1743e123c87d8b176c0ea292a584fb99292d05
SweatierKey <cubolotto02@gmail.com>
2025-11-17 13:37:27 +0100

    push

--- modules/packages/terminal-emulators.scm
+++ modules/packages/terminal-emulators.scm
@@ -304,7 +304,7 @@
                              (uri "https://github.com/mbadolato/iTerm2-Color-Schemes/releases/download/release-20251110-150531-d5f3d53/ghostty-themes.tgz")
                              (sha256
                                (base32
-                                 "0z1a82gv67851x0bf5hwplkra45s2pj26ydkzdx3lcrhvrb1dh8s")))))
+                                 "kwnlol7xaabo5zbmbzukrfqnrqdebjug4v6bwqgn65ffj24jhuba")))))
                         ("N-V-__8AAH0GaQC8a52s6vfIxg88OZgFgEW6DFxfSK4lX_l3"
                          (ungexp
                            (origin
