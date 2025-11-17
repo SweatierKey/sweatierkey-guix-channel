da1e794f1ea1583ca0cb7d8cb2395b250f02d00f
SweatierKey <cubolotto02@gmail.com>
2025-11-17 10:37:18 +0100

    push

--- modules/packages/terminal-emulators.scm
+++ modules/packages/terminal-emulators.scm
@@ -2,7 +2,7 @@
 ;;;
 ;;; This file is NOT part of GNU Guix.
 
-(define-module (module packages terminals-emulators)
+(define-module (modules packages terminals-emulators)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages base)
   #:use-module (gnu packages compression)
